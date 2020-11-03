31fe2ae4751acdc939d47a6589da05d691b6f032
Mira Sohn <tonton07@gmail.com>
2020-11-03 13:25:11 -0500

    Bash file executing STAR indexing

---
+++ STAR-ensembl/STARindex.sh
@@ -0,0 +1,20 @@
+#!bin/bash 
+
+# Source of reference:
+# FASTA: https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/ ---> hg19.fa.gz
+# GTF: https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/genes/  ---> hg19.ensGene.gtf.gz 
+
+# For more info, check this out: 
+# https://genome.ucsc.edu/FAQ/FAQgenes.html#hg19
+
+
+fasta="hg19.fa"
+
+gtf="hg19.ensGene.gtf"
+
+mkdir genomegen
+
+STAR --runThreadN 8 --runMode genomeGenerate --genomeDir ./genomegen --genomeFastaFiles $fasta --sjdbGTFfile $gtf 
+
+
+
